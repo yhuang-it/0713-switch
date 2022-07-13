3899771cf0f46a171cf7e5c6bc1c8c724ea27811
yhuang <yhuang.it@gmail.com>
2022-07-13 10:41:06 -0400

    main

---
+++ .vscode/switch.js
@@ -0,0 +1,26 @@
+let today = 'Saturday';
+ 
+switch (today) {
+  case 'Monday':
+    console.log('Today is Monday');
+    break;
+  case 'Tuesday':
+    console.log('Today is Tuesday');
+    break;
+  case 'Wednesday':
+    console.log('Today is Wednesday');
+    break;
+  case 'Thursday':
+    console.log('Today is Thursday');
+        break;
+  case 'Friday':
+    console.log('Today is Friday');
+        break;
+  case 'Saturday':
+    console.log('Today is Saturday');
+        break;
+
+  default:
+    console.log('Today is Sunday');
+    break;
+}
